2b52de35899d9bd98d2c152ed9a89a485ab88c32
Bakhtiyor <baxafxpro22@gmail.com>
2025-07-03 17:26:00 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
      <AppBody/>
-     <AppCard img="./src/assets/illustration-article.svg" button="Learning" text="HTML & CSS foundations" pTag="Published 21 Dec 2023" TagP='These languages are the backbone of every website, defiring structure, content, and pressentation. '></AppCard>
+     <AppCard img="./src/assets/illustration-article.svg" button="Learning" text="HTML & CSS foundations" pTag="Published 21 Dec 2023" TagP='These languages are the backbone of every website, defiring structure, content, and pressentation. ' H1Tag='Greg Hooper'></AppCard>
      
     </>
   )
--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -6,7 +6,8 @@ function AppCard(props){
           <button style={{position:'relative',left:'10px',top:'20px',textAlign:'center',width:'80px',height:'30px',border:'none',borderRadius:'10px',cursor:'pointer',backgroundColor:'hsl(47, 88%, 63%)'}}>{props.button}</button>
           <p style={{position:'relative',left:'10px',top:'10px',fontFamily:'sans-serif',fontWeight:"200",fontSize:'20px',color:"hsl(0, 0%, 42%)"}}>{props.pTag}</p>
           <h1 style={{position:'relative', top:'-10px',cursor:'pointer',left:'10px',fontFamily:"sans-serif",fontWeight:'600',color:'hsl(47, 88%, 63%)'}}>{props.text}</h1>
-          <p style={{position:'relative',left:'10px', wordBreak:'break-word',fontFamily:'paragraph',fontSize:'20px'}}>{props.TagP}</p>
+          <p style={{position:'relative',top:'-20px',left:'10px', wordBreak:'break-word',fontFamily:'sans-serif',fontSize:'22px',color:'hsl(0, 0%, 42%)'}}>{props.TagP}</p>
+          <div className='avat'> <img style={{width:'40px',height:'40px'}} src="./src/assets/image-avatar.webp" alt="" /> <h3 >{props.H1Tag}</h3> </div>
         </div>
     )
 }
